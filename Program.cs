f26b6deb0b9afe38bbcac0cf05029897f9fb3ff6
Chiao <luck_chiao@yahoo.com.tw>
2021-08-24 15:48:07 +0800

    feat: add sj dotnet login sample code

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Sinopac.Shioaji;
+
+namespace dotnet_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shioaji _api = new Shioaji();
+            var _accounts = _api.Login("", "");
+            Console.WriteLine(_accounts);
+        }
+    }
+}
